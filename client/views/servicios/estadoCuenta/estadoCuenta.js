1244a3fb8d790d3a627e2c55f51c6022ce030684
Omar Andres Landi <olandi@modernizacion.gob.ar>
2019-11-06 11:15:33 -0300

    Update code

--- client/views/servicios/estadoCuenta/estadoCuenta.js
+++ client/views/servicios/estadoCuenta/estadoCuenta.js
@@ -9,14 +9,13 @@ Template.inicio.helpers({
   estadoCuenta: function () {
     let settings = new StructurePanel()
     settings.id = 'estadoCuenta'
-    settings.status = Session.get('statusProfile') && Session.get('statusProfile').response ?'primary' : settings.status
+    settings.status = 'none'//Session.get('statusProfile') && Session.get('statusProfile').response ?'primary' : settings.status
     settings.name = 'Estado cuenta'
     settings.service = Session.get('statusProfile')
     settings.bodyTemplate = 'estadoCuenta'
     settings.action = {
-      close: false,
-      collapse: false,
-      collapsed: false
+      name: '', //'collapse', // 'close'
+      currentStatus: false
     }
     return settings
   }
